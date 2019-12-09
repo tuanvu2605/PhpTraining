06a9650b020d186dfa9435d6bf75d708bfe714ed
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:55:09 +0700

    Create User_Model.php

---
+++ MVCbase2/model/User_Model.php
@@ -0,0 +1,36 @@
+<?php
+class User_Model
+{
+    protected $conn = null;
+    public function data()
+    {
+        return
+            [
+                [
+                    'name' => 'Brennan',
+                    'age' => '13',
+                    'email' => 'brennanNgo@gmail.com',
+                    'gender' => 'male',
+                ],
+                [
+                    'name' => 'Angeland',
+                    'age' => '11',
+                    'email' => 'angeland@gmail.com',
+                    'gender' => 'male',
+                ],
+                [
+                    'name' => 'Brennan',
+                    'age' => '13',
+                    'email' => 'brennanNgo@gmail.com',
+                    'gender' => 'male',
+                ],
+                [
+                    'name' => 'Brennan',
+                    'age' => '13',
+                    'email' => 'brennanNgo@gmail.com',
+                    'gender' => 'male',
+                ],
+
+            ];
+    }
+}
