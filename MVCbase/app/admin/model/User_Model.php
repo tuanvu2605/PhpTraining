5a8b0ce23d0d558182fac652f87d59e6e3704f7d
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:54:13 +0700

    Create User_Model.php

---
+++ MVCbase/app/admin/model/User_Model.php
@@ -0,0 +1,16 @@
+<?php
+defined('PATH_SYSTEM') or die('Bad requested');
+
+require_once(PATH_SYSTEM.'/core/FT_Model.php');
+
+class User_Model extends FT_Model
+{
+    function __construct()
+    {
+        parent:: __construct();
+    }
+
+}
+
+
+?>
