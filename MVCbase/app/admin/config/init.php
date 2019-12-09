ebe8591d75271debe847a157b9a676ccf04e2c54
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:54:04 +0700

    Create init.php

---
+++ MVCbase/app/admin/config/init.php
@@ -0,0 +1,14 @@
+<?php
+defined('PATH_SYSTEM') or die('Bad requested!');
+
+return array
+(
+    'default_controller'    => 'index', // controller mặc định
+    'default_action'        => 'index', // action mặc định
+    '404_controller'        => 'error', // controller lỗi 404
+    '404_action'            => 'index'  // action lỗi 404
+)
+
+
+
+?>
