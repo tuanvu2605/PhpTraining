56c04a15b680e8d658b8463f49f2f460f8610c16
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:54:28 +0700

    Create config.php

---
+++ MVCbase/app/system/config/config.php
@@ -0,0 +1,8 @@
+<!-- chua cac hang so dinh nghia su dung trong he thong -->
+<?php
+//Thong so database
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','app')
+?>
