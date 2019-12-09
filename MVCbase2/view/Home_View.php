1b9f851fd023fb39e53024d5076252683986bfe3
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:55:16 +0700

    Create Home_View.php

---
+++ MVCbase2/view/Home_View.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <table class="table table-striped table-inverse table-responsive">
+        <thead class="thead-inverse">
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Email</th>
+                <th>Gender</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php echo $data?>
+            </tbody>
+    </table>
+    <?php
+
+    ?>
+</body>
+</html>
