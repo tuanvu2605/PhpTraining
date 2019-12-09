15a61633398feb663a7ede33f0befd59f2e31486
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:54:01 +0700

    Create config.php

---
+++ MVCbase/app/admin/config/config.php
@@ -0,0 +1,10 @@
+<?php
+defined('PATH_SYSTEM') or die('Bad requested');
+
+//Thiet lap thong so 
+return 
+[
+    'csrf_token_name' => 'tokenname',
+    'base_url' => 'http://localhost/MVCbase',
+];
+?>
