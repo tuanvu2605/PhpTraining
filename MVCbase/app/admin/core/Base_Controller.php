72f37f635549be48d0b5701aa2f01b450ef582c1
Anh Ngo <57741095+brennanngo1292000@users.noreply.github.com>
2019-12-09 11:54:09 +0700

    Create Base_Controller.php

---
+++ MVCbase/app/admin/core/Base_Controller.php
@@ -0,0 +1,12 @@
+<?php
+// defined(PATH_SYSTEM) or die('Bad requested');
+
+class Base_Controller extends FT_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+
